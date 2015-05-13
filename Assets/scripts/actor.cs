b034168be8728fbddcbfb810a2758f699d2b8f71
Adrian S. Leven <adrianleven1000@gmail.com>
2015-05-13 10:35:36 -0700

    new file:   Assets/_prefab.meta 	new file:   Assets/_prefab/left door.prefab 	new file:   Assets/_prefab/left door.prefab.meta 	new file:   Assets/_prefab/playa.prefab 	new file:   Assets/_prefab/playa.prefab.meta 	new file:   Assets/_prefab/right door.prefab 	new file:   Assets/_prefab/right door.prefab.meta 	new file:   Assets/_scenes.meta 	new file:   Assets/_scenes/tinygame.unity 	new file:   Assets/_scenes/tinygame.unity.meta 	new file:   Assets/door.png 	new file:   Assets/door.png.meta 	new file:   Assets/door1.png 	new file:   Assets/door1.png.meta 	new file:   Assets/door2.png 	new file:   Assets/door2.png.meta 	new file:   Assets/protag.png 	new file:   Assets/protag.png.meta 	new file:   Assets/scripts.meta 	new file:   Assets/scripts/actor.cs 	new file:   Assets/scripts/actor.cs.meta 	new file:   Assets/scripts/game_manager.cs 	new file:   Assets/scripts/game_manager.cs.meta 	new file:   Assets/scripts/room_manager.cs 	new file:   Assets/scripts/room_manager.cs.meta

---
+++ Assets/scripts/actor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class actor : MonoBehaviour {
+	public float move_time = .01f;
+	public LayerMask blockingLayer;
+
+	private BoxCollider2D box_collider;
+	private Rigidbody2D rb2;
+	private float inverse_move_time;
+	// Use this for initialization
+	protected virtual void Start () {
+		box_collider = GetComponent<BoxCollider2D> ();
+		rb2 = GetComponent<Rigidbody2D> ();
+		inverse_move_time = 1f / move_time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/room_manager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class room_manager : MonoBehaviour {
+	
+	public int door_pos;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
